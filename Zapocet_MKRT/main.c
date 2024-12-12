e4b489ad45ca5cfcbb1aa843d5b363fc5bd0b5ff
_tuse <75646496+tusetuse@users.noreply.github.com>
2024-12-12 01:05:16 +0100

    Update main.c

--- Zapocet_MKRT/main.c
+++ Zapocet_MKRT/main.c
@@ -2,11 +2,11 @@
 #include "c8051f330.h"
 
 const char *code morse_table[36] = {
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",   // A-J
-    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",     // K-T
-    "..-", "...-", ".--", "-..-", "-.--", "--..",                           		// U-Z
+    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",   	// A-J
+    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",     	// K-T
+    "..-", "...-", ".--", "-..-", "-.--", "--..",                           	// U-Z
     "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...",  	// 0-7
-    "---..", "----."                                                       			// 8-9
+    "---..", "----."                                                       	// 8-9
 };
 
 extern void Init_Device(void);
@@ -18,34 +18,34 @@ void UART_Transmit(char c) {				//funkcia nacita a transmitne jednotlive znaky m
     SBUF0 = c;
 }
 
-void Send_Morse_UART(char *morse) {				//funkcia podla indexu najde ekvivalent morsovho kodu pre jednotlive pismena/cisla v poli a passne ich do Transmit funkcie
+void Send_Morse_UART(char *morse) {			//funkcia podla indexu najde ekvivalent morsovho kodu pre jednotlive pismena/cisla v poli a passne ich do Transmit funkcie
 	
-    while (*morse) {																			//Loopuje pokial nehitne null character(koniec stringu) = koniec pismena/cisla v morse kode
-        UART_Transmit(*morse++);								//Nacita jeden znak z pismena/cisla morse kodu a prejde na dalsi 
+    while (*morse) {					//Loopuje pokial nehitne null character(koniec stringu) = koniec pismena/cisla v morse kode
+        UART_Transmit(*morse++);			//Nacita jeden znak z pismena/cisla morse kodu a prejde na dalsi 
     }
-    UART_Transmit(' ');																//Nacita medzeru ked loop skonci
+    UART_Transmit(' ');					//Nacita medzeru ked loop skonci
 }
 
 void main() {
-		char *ptr;
-		char ch;
-		char input[64];
-		Init_Device();
-		TR1 = 1;              // Start Timer 
-    TI0 = 1; 							// Nastavenie transmit interupt flag na prvy prenos
+	char *ptr;		
+	char ch;
+	char input[64];
+	Init_Device();
+	TR1 = 1;              				// Start Timer 
+	TI0 = 1; 					// Nastavenie transmit interupt flag na prvy prenos
 
     while (1) {
         printf("Enter a message: ");
-        scanf("%63s", input);								//Obmedzi user input na 63 znakov
+        scanf("%63s", input);				//Obmedzi user input na 63 znakov
         ptr = input;
-        while (*ptr) {															//Loop bezi pokial pointer nepride na koniec stringu 
+        while (*ptr) {					//Loop bezi pokial pointer nepride na koniec stringu 
             ch = *ptr++;
             if (ch >= 'a' && ch <= 'z') {
-                ch -= 'a';													//Prevod na index 0-25
+                ch -= 'a';				//Prevod na index 0-25
             } else if (ch >= 'A' && ch <= 'Z') {
-                ch -= 'A';													//Prevod na index 0-25
+                ch -= 'A';				//Prevod na index 0-25
             } else if (ch >= '0' && ch <= '9') {
-                ch -= '0' + 26;									//Prevod na index 26-35
+                ch -= '0' + 26;				//Prevod na index 26-35
             } else {
                 continue;
             }
